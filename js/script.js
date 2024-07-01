52049447a8b52c6db3026a92a7aed7451057457e
cheikhfaye123 <baboudieye58@gmail.com>
2024-07-01 21:00:01 +0200

    BooM

--- js/script.js
+++ js/script.js
@@ -1,27 +1,46 @@
+let numberSelect = 0;
 const userInput = document.getElementById('userInput');
 const countdown = document.getElementById('countdown');
 const result = document.getElementById('result');
-const restartButton = document.getElementById('restart');
+const restart = document.getElementById('restart');
 
-let timeLeft = 5;
-let intervalId;
-let randomNumber;
+userInput.addEventListener("change", () => {
+    numberSelect = parseInt(userInput.value);
+});
 
-function startGame() {
-  userInput.disabled = false;
-  result.textContent = '';
-  timeLeft = 5;
-  randomNumber = Math.floor(Math.random() * 3) + 1;
-  
-  return new Promise((resolve) => {
-    intervalId = setInterval(() => {
-      countdown.textContent = timeLeft;
-      if (timeLeft === 0) {
-        clearInterval(intervalId);
-        userInput.disabled = true;
-        resolve();
-      }
-      timeLeft--;
+function iniciarContador() {
+    let segundos = 5;
+    const intervalId = setInterval(() => {
+        if (segundos >= 0) {
+            countdown.innerText = segundos;
+            segundos--;
+        } else {
+            clearInterval(intervalId);
+        }
     }, 1000);
-  });
 }
+
+const getPromise = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            let mensaje = "";
+            const randomNumber = Math.floor(Math.random() * 3) + 1;
+            if (numberSelect === randomNumber) {
+                mensaje = `<h2>¡Has salvado el mundo!</h2><p>Tu número ${numberSelect} es igual a ${randomNumber}</p>`;
+            } else {
+                mensaje = `<h2>¡La bomba ha explotado!</h2><p>Tu número ${numberSelect} no es igual a ${randomNumber}</p>`;
+            }
+            resolve(mensaje);
+        }, 6000);
+    });
+};
+
+function iniciarJuego() {
+    result.innerHTML = '';
+    iniciarContador();
+    getPromise().then(data => result.innerHTML = data);
+}
+
+restart.addEventListener('click', iniciarJuego);
+
+iniciarJuego();
